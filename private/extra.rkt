eb54d58209db422be87c09071dada21bf85a7b6f
yjqww6 <343519265@qq.com>
2020-08-01 19:38:37 +0800

    improve

--- private/extra.rkt
+++ private/extra.rkt
@@ -12,7 +12,6 @@
   (λ (stx)
     (syntax-parse stx
       [[(Id:id) (_ S:expr)]
-       #:with (next x tmp) (generate-temporaries '(next x tmp))
        (syntax-parse (expand-for-clause #'S #'[(tmp) S])
          [(([(outer-id ...) outer-expr] ...)
            outer-check
@@ -22,48 +21,36 @@
            pre-guard
            post-guard
            (loop-arg ...))
-          #:do [(define loop-set (for/fold ([b (immutable-bound-id-set (list #'x))])
-                                           ([id (in-syntax #'(loop-id ...))])
-                                   (bound-id-set-add b id)))]
-          #:with (inner-loop-id ...) (filter (λ (id)
-                                               (not (bound-id-set-member? loop-set id)))
-                                             (syntax->list #'(inner-id ... ...)))
-          #:with (falsy ...) (stx-map (λ (_) #'#f) #'(inner-id ... ...))
           #'[(Id)
              (:do-in
-              ([(next outer-id ... ...)
-                (let-values ([(outer-id ...) outer-expr] ...)
-                  outer-check
-                  (define (next loop-id ...)
-                    (if pos-guard
-                        (let-values ([(inner-id ...) inner-expr]
-                                     ...)
-                          (if pre-guard
-                              (begin
-                                (unless (list? tmp)
-                                  (raise-argument-error
-                                   'Id "list?" tmp))
-                                (if (null? tmp)
-                                    (if post-guard
-                                        (next loop-arg ...)
-                                        (values #f #f falsy ...))
-                                    (values (unsafe-car tmp)
-                                            (unsafe-cdr tmp)
-                                            inner-id ... ...)))
-                              (values #f #f falsy ...)))
-                        (values #f #f falsy ...)))
-                  
-                  (values next outer-id ... ...))])
-              (void)
-              ([x '()] [loop-id loop-expr] ...
-                       [inner-loop-id #f] ...)
+              ([(outer-id ...) outer-expr] ...)
+              outer-check
+              ([x '()] [post #t] [loop-id loop-expr] ...)
               #t
-              ([(Id x inner-id ... ...)
-                (if (null? x)
-                    (next loop-id ...)
-                    (values (unsafe-car x) (unsafe-cdr x)
-                            inner-id ... ...))])
-              x
-              (or (not (null? x)) post-guard)
-              (x (if (null? x) loop-arg loop-id) ...
-                 inner-loop-id ...))]])])))
+              ([(Id rst post loop-id ...)
+                (if (not (null? x))
+                    (values (unsafe-car x)
+                            (unsafe-cdr x)
+                            post
+                            loop-id ...)
+                    (if post
+                        (let loop ([loop-id loop-id] ...)
+                          (if pos-guard
+                              (let-values ([(inner-id ...) inner-expr] ...)
+                                (if pre-guard
+                                    (begin
+                                      (unless (list? tmp)
+                                        (raise-argument-error 'tmp "list?" tmp))
+                                      (if (null? tmp)
+                                          (if post-guard
+                                              (loop #f loop-arg ...)
+                                              (values #f #f #f loop-id ...))
+                                          (values (unsafe-car tmp) (unsafe-cdr tmp)
+                                                  post-guard
+                                                  loop-arg ...)))
+                                    (values #f #f #f loop-id ...)))
+                              (values #f #f #f loop-id ...)))
+                        (values #f #f #f loop-id ...)))])
+              rst
+              #t
+              (rst post loop-id ...))]])])))
