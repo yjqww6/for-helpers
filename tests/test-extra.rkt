0b6497badd711aed4d6e19f5dc2e232b8fc80a63
yjqww6 <343519265@qq.com>
2020-08-02 11:17:11 +0800

    more test

--- tests/test-extra.rkt
+++ tests/test-extra.rkt
@@ -8,4 +8,22 @@
    (for/list ([x (in-lists (in-lists (in-list '(((1 2 3) (4 5 6))
                                                 ((7 8 9) (a b c))))))])
      x)
-   '(1 2 3 4 5 6 7 8 9 a b c)))
+   '(1 2 3 4 5 6 7 8 9 a b c))
+
+  (check-equal?
+   (for/list ([x (in-lists
+                  (stop-before (in-list
+                               '((1 2 3) (4 5 6) (7 8 9) (a b c)))
+
+                              (compose1 not integer? car)))])
+     x)
+   '(1 2 3 4 5 6 7 8 9))
+
+  (check-equal?
+   (for/list ([x (in-lists
+                  (stop-after (in-list
+                               '((1 2 3) (4 5 6) (7 8 9) (a b c)))
+
+                              (λ (x) (= (car x) 7))))])
+     x)
+   '(1 2 3 4 5 6 7 8 9)))
